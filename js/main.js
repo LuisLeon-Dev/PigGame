8bdcca56ec4ee893e2bea90110a1fa6e140cd58f
LuisLeon-Dev <lleon5073@gmail.com>
2023-12-21 17:47:11 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+"use stric";
+
+//varuables
+const playerScore0 = document.getElementById("score--0");
+const playerScore1 = document.getElementById("score--1");
+const dice = document.querySelector(".dice");
+
+playerScore0.textContent = 0;
+playerScore1.textContent = 0;
+dice.style.display = "none";
